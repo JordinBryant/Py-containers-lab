644cbb6d448b5fc3d33d077002a60abb14c31b20
Jordin Bryant <jordintaylor095@gmail.com>
2022-10-12 19:19:24 -0400

    initial commit

---
+++ main.py/main.py
@@ -0,0 +1,61 @@
+# EXERCISE 1
+# students = ['Jordin','John','Asha']
+
+# John = students[1]
+# print(John)
+
+# Asha = students[2]
+# print(Asha)
+
+# EXERCISE 2
+# foods = ('soup', 'chicken', 'noodles')
+
+# for food in foods:
+#   print('food string goes here is a good food')
+
+# EXERCISE 3
+
+# chicken = foods[1]
+# noodles = foods[2]
+# print(chicken)
+# print(noodles)
+
+# EXERCISE 4
+
+# home_town = {
+#   'city': 'Detroit',
+#   'state': 'Michigan',
+#   'population': '672,351'
+# }
+# for key, val in home_town.items():
+#   print("I was born in"  f"{key} = {val}" )
+
+#EXERCISE 5
+# for key, val in home_town.items():
+#   print(f"{key} = {val}")
+
+#EXERCISE 6
+# cohort = []
+# # len (cohort)
+# cohort.append('student:')
+# cohort.append('jordin')
+# cohort.append('student:')
+# cohort.append('John')
+# cohort.append('student:')
+# cohort.append('Asha')
+
+# print(cohort)
+
+# EXERCISE 7
+# awesome_students = []
+# for student in students:
+#   awesome_students.append(f'{student} is awesome!')
+#   print(awesome_students)
+
+#EXCERCISE 8
+# a_foods =[]
+
+# if "a" in food:
+#   a_foods.append(foods)
+# for food in a_foods:
+#   print(a_foods)
